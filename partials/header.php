967e4f85dea3f6d0ca1a2ef5c8f6f007f0074876
phamtungthuy <94853261+phamtungthuy@users.noreply.github.com>
2022-11-23 00:36:53 +0700

    header and add loader

--- partials/header.php
+++ partials/header.php
@@ -4,6 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Food Website</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/loader.css">
 </head>
 <body>
+<div class="loader"> 
+   <div class="cssload-container">
+     <div class="cssload-circle"></div>
+     <div class="cssload-circle"></div>
+   </div>
+</div>
+<div class="topbar">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <p class="pull-left hidden-xs">JKS Foods and Restaurant, the Best in Town</p>
+        <p class="pull-right"><i class="fa fa-phone"></i>Order Online +91-892-808-5056</p>
+      </div>
+    </div>
+  </div>
+</div>
