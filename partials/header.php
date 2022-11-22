201820bb38385b6a76e1ea178beafde561a08636
phamtungthuy <94853261+phamtungthuy@users.noreply.github.com>
2022-11-23 00:41:08 +0700

    Add css

--- partials/header.php
+++ partials/header.php
@@ -8,14 +8,16 @@
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="css/loader.css">
+    <link rel="stylesheet" type="text/css" href="css/navigation.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-<div class="loader"> 
+<!-- <div class="loader"> 
    <div class="cssload-container">
      <div class="cssload-circle"></div>
      <div class="cssload-circle"></div>
    </div>
-</div>
+</div> -->
 <div class="topbar">
   <div class="container">
     <div class="row">
@@ -26,3 +28,38 @@
     </div>
   </div>
 </div>
+<header id="main-navigation">
+  <div id="navigation" data-spy="affix" data-offset-top="20">
+    <div class="container">
+      <div class="row">
+      <div class="col-md-12">
+        <nav class="navbar navbar-default">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#fixed-collapse-navbar" aria-expanded="false"> 
+            <span class="icon-bar top-bar"></span> <span class="icon-bar middle-bar"></span> <span class="icon-bar bottom-bar"></span> 
+            </button>
+           <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="logo" class="img-responsive"></a> 
+         </div>
+        
+            <div id="fixed-collapse-navbar" class="navbar-collapse collapse navbar-right">
+              <ul class="nav navbar-nav">
+                <li>
+                   <a href="index.html">Home</a>
+                   
+                </li>
+                <li><a href="food.html">Our Food</a></li>
+                
+                
+                    <li><a href="about.html">About Us</a></li>
+                    <li><a href="faq.html">FAQ</a></li>
+                  
+                <li><a href="./account/register.php">Order Now</a></li>
+                
+              </ul>
+            </div>
+         </nav>
+         </div>
+       </div>
+     </div>
+  </div>
+</header>
