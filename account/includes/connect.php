bfb1c1f153cb5bcc82e4c36a0663612c0177c10c
phamtungthuy <94853261+phamtungthuy@users.noreply.github.com>
2022-11-29 23:59:34 +0700

    change connect.php file

--- account/includes/connect.php
+++ account/includes/connect.php
@@ -1,9 +1,14 @@
 <?php
 session_start();
-$servername = "db_heroku";
-$server_user = "root";
-$server_pass = "";
-$dbname = "food";
+// $servername = "localhost";
+// $server_user = "root";
+// $server_pass = "";
+// $dbname = "food";
+// $port = 3308;
+$servername = "https://onlinefoodweb.herokuapp.com/";
+$server_user = "bhbtmaggqotebo";
+$server_pass = "437343278bc49ea8f4aa3a7d5cfa54993cf960c3e5cd735ed8615519f157e65f";
+$dbname = "d6qdpvng36l4rb";
 $port = 5432;
 $name = $_SESSION['name'];
 $role = $_SESSION['role'];
