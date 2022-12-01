276bce78020a3f17dc3b98b7e770e36660045d6e
phamtungthuy <94853261+phamtungthuy@users.noreply.github.com>
2022-12-01 08:56:19 +0700

    fix database

--- account/includes/connect.php
+++ account/includes/connect.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
-// $servername = "localhost";
-// $server_user = "root";
-// $server_pass = "";
-// $dbname = "food";
-// $port = 3308;
-$servername = "sql.freedb.tech";
-$server_user = "freedb_phamtungthuy";
-$server_pass = "g#6#fXWeRdT4UJQ";
-$dbname = "freedb_onlinefood";
-$port = 3306;
+$servername = "localhost";
+$server_user = "root";
+$server_pass = "";
+$dbname = "food";
+$port = 3308;
+// $servername = "sql.freedb.tech";
+// $server_user = "freedb_phamtungthuy";
+// $server_pass = "g#6#fXWeRdT4UJQ";
+// $dbname = "freedb_onlinefood";
+// $port = 3306;
 $name = $_SESSION['name'];
 $role = $_SESSION['role'];
 $con = new mysqli($servername, $server_user, $server_pass, $dbname, $port);
